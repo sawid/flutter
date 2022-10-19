ee1e8c0ccca39bea8fbb742f6cd56d3075c0eae9
sawid <zasawidza@gmail.com>
2022-10-20 01:18:37 +0700

    update center widget

---
+++ android/app/src/main/kotlin/com/example/medicare_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medicare_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Titlee")),
+        ),
+        body: Center(child: Text("body ka")),
+      ),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
